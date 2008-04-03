48c5aeeed89bf2b4e1a59b0f67651a2f88bf73d7
Luke Hoersten <Luke@Hoersten.org>
2008-04-02 20:47:42 -0400

    Initial commit

---
+++ ReentrantLockPseudoRandom.java
@@ -0,0 +1,21 @@
+
+public class ReentrantLockPseudoRandom extends PseudoRandom {
+    private final Lock lock = new ReentrantLock(false);
+    private int seed;
+
+    ReentrantLockPseudoRandom (int seed) {
+        this.seed = seed;
+    }
+
+    public int nextInt (int n) {
+        lock.lock();
+        try {
+            int s = seed;
+            seed = calculateNext(s);
+            int remainder = s % n;
+            return remainder > 0 ? remainder : remainder + n;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
